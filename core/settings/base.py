ffe6f7f5ba96f318feacc7dc6ebe7b9c13507788
mbrav <mbrav@protonmail.com>
2022-01-06 22:52:50 +0300

    rename to core

--- core/settings/base.py
+++ core/settings/base.py
@@ -24,22 +24,22 @@ BASE_DIR = os.path.dirname(PROJECT_DIR)
 # Application definition
 
 INSTALLED_APPS = [
-    'apps.home',
+    'home',
     'apps.search',
     'apps.customers',
     'apps.warehouse',
     'apps.orders',
 
-    # 'wagtail.contrib.forms',
-    # 'wagtail.contrib.redirects',
-    # 'wagtail.embeds',
-    # 'wagtail.sites',
-    # 'wagtail.users',
-    # 'wagtail.snippets',
-    # 'wagtail.documents',
-    # 'wagtail.images',
-    # 'wagtail.search',
-    # 'wagtail.admin',
+    'wagtail.contrib.forms',
+    'wagtail.contrib.redirects',
+    'wagtail.embeds',
+    'wagtail.sites',
+    'wagtail.users',
+    'wagtail.snippets',
+    'wagtail.documents',
+    'wagtail.images',
+    'wagtail.search',
+    'wagtail.admin',
     'wagtail.core',
 
     'modelcluster',
