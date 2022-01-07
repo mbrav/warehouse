8b8cff6c479f1c0f92c999374e79c94081854c6a
mbrav <mbrav@protonmail.com>
2022-01-07 22:24:28 +0300

    add django-debug-toolbar

--- core/settings/dev.py
+++ core/settings/dev.py
@@ -9,6 +9,15 @@ SECRET_KEY = 'django-insecure-ijcmyjm9gl5cckfjxk2c4)3*zzipvxrr(npov*e5=b_ig)_vpy
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*']
 
+# Django Debug toolbar
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+INSTALLED_APPS.extend(('debug_toolbar',))
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1',
+    # ...
+]
+
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 
--- core/urls.py
+++ core/urls.py
@@ -18,12 +18,15 @@ urlpatterns = [
 
 
 if settings.DEBUG:
+    import debug_toolbar
     from django.conf.urls.static import static
     from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
     # Serve static and media files from development server
+    urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
     urlpatterns += staticfiles_urlpatterns()
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
 
 urlpatterns = urlpatterns + [
     # For anything not caught by a more specific rule above, hand over to
